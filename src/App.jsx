0073b7794e4edd714607d064b018628199848eb8
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-08-21 14:45:57 +0200

    Aggiunta Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 
-function App() {
-  const products = [
+const products = [
   { name: 'Mela', price: 0.5 },
   { name: 'Pane', price: 1.2 },
   { name: 'Latte', price: 1.0 },
   { name: 'Pasta', price: 0.7 },
 ];
 
+function App() {
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  function addToCart(product) {
+    const productAlreadyInCart = addedProducts.some(
+      (cartProduct) => cartProduct.name === product.name
+    );
+
+    if (productAlreadyInCart) {
+      return;
+    }
+
+    const productWithQuantity = {
+      ...product,
+      quantity: 1,
+    };
+
+    setAddedProducts([
+      ...addedProducts,
+      productWithQuantity,
+    ]);
+  }
+
   return (
     <main className="container">
       <h1>Carrello della spesa</h1>
 
       <section>
-        <h2>Prodotti disponibili</h2>
+        <h2>Lista dei prodotti</h2>
 
         <ul className="product-list">
           {products.map((product) => (
             <li className="product-card" key={product.name}>
               <div>
                 <h3>{product.name}</h3>
-                <p>{product.price.toFixed(2)} €</p>
+                <p>Prezzo: {product.price.toFixed(2)} €</p>
               </div>
 
               <button onClick={() => addToCart(product)}>
@@ -31,8 +53,29 @@ function App() {
         </ul>
       </section>
 
-      </main>
-  )
+      {addedProducts.length > 0 && (
+        <section className="cart-section">
+          <h2>Prodotti nel carrello</h2>
+
+          <ul className="cart-list">
+            {addedProducts.map((product) => (
+              <li className="cart-card" key={product.name}>
+                <h3>{product.name}</h3>
+
+                <p>
+                  Prezzo: {product.price.toFixed(2)} €
+                </p>
+
+                <p>
+                  Quantità: {product.quantity}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
